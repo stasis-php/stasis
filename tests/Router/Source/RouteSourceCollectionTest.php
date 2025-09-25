c64d882d1bd2a239bc38d1c47fa2c307a93bbc45
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2025-09-25 23:12:47 +0200

    add route source collection test

---
+++ tests/Router/Source/RouteSourceCollectionTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stasis\Tests\Router\Source;
+
+use PHPUnit\Framework\TestCase;
+use Stasis\Exception\LogicException;
+use Stasis\Router\Route\Route;
+use Stasis\Router\Source\RouteSource;
+use Stasis\Router\Source\RouteSourceCollection;
+
+class RouteSourceCollectionTest extends TestCase
+{
+    public function testAdd(): void
+    {
+        $collection = new RouteSourceCollection();
+
+        $route1 = new Route('/page1', 'reference_1');
+        $route2 = new Route('/page2', 'reference_2');
+        $route3 = new Route('/page3', 'reference_3');
+
+        $source1 = new RouteSource('source1', [$route1]);
+        $source2 = new RouteSource('source2', [$route2, $route3]);
+
+        $collection->add($source1);
+        $collection->add($source2);
+
+        $iterated = iterator_to_array($collection);
+
+        self::assertSame([$route1, $route2, $route3], $iterated, 'Unexpected routes returned');
+    }
+
+    public function testAddDuplicateNameThrows(): void
+    {
+        $collection = new RouteSourceCollection();
+        $route = new Route('/page', 'reference');
+
+        $collection->add(new RouteSource('duplicated', [$route]));
+
+        $this->expectException(LogicException::class);
+        $this->expectExceptionMessage('Route source "duplicated" already exists.');
+        $collection->add(new RouteSource('duplicated', [$route]));
+    }
+}
