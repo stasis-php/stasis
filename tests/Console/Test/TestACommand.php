d2a21d6f0cd4448c6dc699ac8bd98356df9dc086
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2025-11-01 13:41:14 +0100

    add command loader test

---
+++ tests/Console/Test/TestACommand.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stasis\Tests\Console\Test;
+
+use Stasis\Console\CommandFactoryInterface;
+use Stasis\Kernel;
+use Symfony\Component\Console\Command\Command;
+
+class TestACommand extends Command implements CommandFactoryInterface
+{
+    private const string NAME = 'test:a';
+
+    public static function name(): string
+    {
+        return self::NAME;
+    }
+
+    public static function description(): string
+    {
+        return 'Test command A';
+    }
+
+    public static function create(Kernel $kernel): Command
+    {
+        return new self(self::NAME);
+    }
+}
